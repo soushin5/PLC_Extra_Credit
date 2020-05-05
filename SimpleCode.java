a09446d64f0d6688a406c8a0a11e0aa782625cda
soushin5 <38273387+soushin5@users.noreply.github.com>
2020-05-04 20:34:50 -0400

    Update SimpleCode.java

--- SimpleCode.java
+++ SimpleCode.java
@@ -1,6 +1,11 @@
-/* PLC Final question 1:
- * Write code for a state diagram to recognize the floating-point literals, integer literals, string
- * literals and variable names of any programming language that was developed after 1990
+/* BNF: SimpleCode: Boolean, Assignment, Math
+ * <assign> -> <assign> = <logic>| <logic>
+ * <logic>  -> <logic> && <equal> | <logic> || <equal> | <equal>          
+ * <equal>  -> <equal> == <rel> | <equal> != <rel> | <rel>
+ * <rel>    -> <rel> < <add> | <rel> <= <add> | <rel> > <add> | <rel> >= <add> | <add>
+ * <add>    -> <add> + <mult> | <add> - <mult> | <mult>
+ * <mult>   -> <mult> * <term> | <mult> / <term> | <mult> % <term> | <term>
+ * <term>   -> (<add>) | int_lit | id
  */
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,19 +22,6 @@ public class SimpleCode {
 	static File in_fp;
 	static int states = 0;
 
-	//States
-	static final int START = 0;
-	static final int NUM = 1;
-	static final int FL_NUM = 2;
-	static final int STR = 3;
-	
-	//End states
-	static final int END_N = 4;
-	static final int END_F = 5;
-	static final int END_S = 6;
-	static final int END_I = 7;
-	static final int END_E = 8;
-
 	//Char Types
 	static final int LETTER = 0;
 	static final int DIGIT = 1;
@@ -37,33 +29,35 @@ public class SimpleCode {
 	static final int EOF = -1;
 
 	//char values
-	static final int STR_LIT = 10;
-	static final int IDENT = 11;
-	static final int INT_LIT = 12;
-	static final int dot = 13;
-	static final int FLO_LIT = 14;
-    static final int QUOTES = 15;
-    
-    //special characters
-    static int Flag = 0;
-    
-	static public void state(int states) {
-		switch(states) {
-		case END_S:
-			System.out.println("End state is: " + states + ", a String Literal.");
-			break;
-		case END_I:
-			System.out.println("End state is: " + states + ", a Variable Name");
-			break;
-		case END_N:
-			System.out.println("End state is: " + states + ", an Integer Literal.");
-			break;
-		case END_F:
-			System.out.println("End state is: " + states + ", a Floating Point Literal.");
-			break;
-		}
-	}
-    static int lookup(char ch){
+	static final int STR_LIT = 1;
+	static final int IDENT = 2;
+	static final int INT_LIT = 3;
+	static final int FLO_LIT = 4;
+	static final int dot = 5;
+
+	static final int QUOTES = 6;
+	static final int DUB_EQ = 10;
+	static final int NOT_EQ = 11;
+	static final int EQ = 12;
+	static final int LE = 13;
+	static final int GR = 15;
+	static final int LE_EQ = 16;
+	static final int GR_EQ = 17;
+	static final int ADD_OP = 18;
+	static final int SUB_OP = 19;
+	static final int MULT_OP = 20;
+	static final int DIV_OP = 21;
+	static final int MOD_OP = 22;
+	static final int L_PARE = 25;
+	static final int R_PARE = 26;
+	static final int AND_OP =27;
+	static final int OR_OP = 28;
+
+	//special characters
+	static int Flag = 0;
+
+
+	static int lookup(char ch){
 		switch (ch) {
 		case '.':
 			addChar();
@@ -72,6 +66,34 @@ public class SimpleCode {
 		case '"':
 			addChar();
 			nextTok = QUOTES;
+			break;
+		case '(':
+			addChar();
+			nextTok = L_PARE;
+			break;
+		case ')':
+			addChar();
+			nextTok = R_PARE;
+			break;
+		case '+':
+			addChar();
+			nextTok = ADD_OP;
+			break;
+		case '-':
+			addChar();
+			nextTok = SUB_OP;
+			break;
+		case '*':
+			addChar();
+			nextTok = MULT_OP;
+			break;
+		case '/':
+			addChar();
+			nextTok = DIV_OP;
+			break;
+		case '%':
+			addChar();
+			nextTok = MOD_OP;
 			break;
 		default:
 			addChar();
@@ -103,27 +125,24 @@ public class SimpleCode {
 					Flag = 2;
 				}
 				charClass = LETTER;
-				states = STR;
 			}
 			else if (Character.isDigit(nextChar)) {
 				charClass = DIGIT;
-				states = NUM;
 			}
 			else {
 				charClass = UNKNOWN;
 			}
 		} else {
 			charClass = EOF;
-			states = END_E;
 		}
 	}
+
 	static void getNonBlank(BufferedReader br) throws IOException {
 		while (Character.isWhitespace(nextChar)) {
 			getChar(br);
-			}
-		states = START;
-		
+		}
 	}
+
 	static int lex(BufferedReader br) throws IOException {
 		lexeme = new char[lexeme.length];
 		lexLen = 0;
@@ -138,9 +157,8 @@ public class SimpleCode {
 				getChar(br);
 			}
 			nextTok = IDENT;
-			states = END_I;
 			break;
-		/* Integer literals and Float Literals*/
+			/* Integer literals and Float Literals*/
 		case DIGIT:
 			addChar();
 			getChar(br);
@@ -161,14 +179,12 @@ public class SimpleCode {
 						getChar(br);
 					}
 					nextTok = FLO_LIT;
-					states = END_F;
 					break;
 				}		
 			}
 			nextTok = INT_LIT;
-			states = END_N;
 			break;
-		/*quotes for Strings*/
+			/*quotes for Strings*/
 		case UNKNOWN:
 			lookup(nextChar);
 			if(nextTok == QUOTES) {
@@ -180,7 +196,6 @@ public class SimpleCode {
 				addChar();
 				getChar(br);
 				nextTok = STR_LIT;	
-				states = END_S;
 			}
 			if(nextTok == dot) {
 				getChar(br);
@@ -193,13 +208,11 @@ public class SimpleCode {
 					getChar(br);
 				}
 				nextTok = FLO_LIT;
-				states = END_F;
 			}
 			break;
 			/* EOF */
 		case EOF:
 			nextTok = 0;
-			states = END_E;
 			lexeme[0] = 0;
 			break;
 		} /* End of switch */
@@ -208,23 +221,92 @@ public class SimpleCode {
 			System.out.print(lexeme[i]);
 		}
 		System.out.print("\n");
-		state(states);
 		System.out.print("\n");
 		return nextTok;
 	}
+
+	public static void assign(BufferedReader br) throws IOException {
+		logic(br);
+		while (nextTok == EQ) {
+			lex(br);
+			logic(br);
+		}
+	}
+
+	public static void logic(BufferedReader br) throws IOException {
+		equal(br);
+		while (nextTok == AND_OP || nextTok == OR_OP) {
+			lex(br);
+			equal(br);
+		}
+	}
+	public static void equal(BufferedReader br) throws IOException {
+		rel(br);
+		while (nextTok == DUB_EQ || nextTok == NOT_EQ) {
+			lex(br);
+			rel(br);
+		}
+	}
+
+	public static void rel(BufferedReader br) throws IOException {
+		add(br);
+		while (nextTok == LE || nextTok == LE_EQ || nextTok == GR || nextTok == GR_EQ) {
+			lex(br);
+			add(br);
+		}
+	}
+
+	public static void add(BufferedReader br) throws IOException {
+		mult(br);
+		while (nextTok == ADD_OP || nextTok == SUB_OP) {
+			lex(br);
+			mult(br);
+		}
+	}
+	public static void mult(BufferedReader br) throws IOException {
+		term(br);
+		while (nextTok == MULT_OP || nextTok == DIV_OP || nextTok == MOD_OP) {
+			lex(br);
+			term(br);
+		}
+	}
+
+	public static void term(BufferedReader br) throws IOException {
+		if (nextTok == IDENT || nextTok == INT_LIT) {
+			lex(br);
+		} else {
+			if (nextTok == L_PARE) {
+				lex(br);
+				add(br);
+				if (nextTok == R_PARE) {
+					lex(br);
+				}
+				else {
+					error();
+				}
+			} else
+				error();
+		}
+	}
+	
+	static void error() {
+		System.out.println("Error = Symbol not found");
+	}
+
 	public static void main(String[]args) throws IOException{
 		System.out.println("State Diagram Recognition");
 		if ((in_fp = new File("C:\\Users\\kuwan_000\\Documents"
-		          + "\\Georgia State\\GA State Spring 2020\\Programming Language Concepts\\final_1test.txt")) == null) {
+				+ "\\Georgia State\\GA State Spring 2020\\Programming Language Concepts\\final_1test.txt")) == null) {
 			System.out.println("ERROR - cannot open file \n");
 		}
-		 else {
+		else {
 			BufferedReader br = new BufferedReader(new FileReader(in_fp));
-		 	getChar(br);
-		 do {
-		 	lex(br);
-		 } while (nextTok != 0);
-		 br.close();
-		 }
+			getChar(br);
+			do {
+				lex(br);
+				assign(br);
+			} while (nextTok != 0);
+			br.close();
+		}
 	}	
 }
